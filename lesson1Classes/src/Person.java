91c1dc469b247c90b79372dd0868fe070177ddbc
Tums-DMN <59483232+Tums-DMN@users.noreply.github.com>
2020-01-25 01:32:06 +0300

    first update

--- lesson1Classes/src/Person.java
+++ lesson1Classes/src/Person.java
@@ -2,6 +2,7 @@ public class Person {
     private final boolean man;
     private final String name;
     private Person spouse;
+
     public Person(boolean man, String name) {
         this.man = man;
         this.name = name;
@@ -10,19 +11,49 @@ public class Person {
     /**
      * This method checks gender of persons. If genders are not equal - tries to marry.
      * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     *
      * @param person - new husband/wife for this person.
      * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
      */
     public boolean marry(Person person) {
-        ...
+        if ((person.man != this.man) & (person.spouse == null) & (this.spouse == null)) {
+            this.spouse = person;
+            person.spouse = this;
+            return true;
+        } else return false;
     }
+
 
     /**
      * Sets spouse = null if spouse is not null
+     *
      * @return true - if person status has been changed
      */
-    public boolean divorce() {… }
+    public boolean divorce() {
+        if (this.spouse != null){
+            spouse.spouse = null;
+            this.spouse = null;
+            return true;
+        }
+        else return false;
+    }
+
     public static void main(String[] args) {
 
+        Person first = new Person(true, "Ivan");
+        Person second = new Person(false, "Maria");
+        Person third = new Person(false, "Anna");
+
+        if (first.marry(second)) System.out.println(first.name + " & " + second.name + " Marryed!!!");
+            else System.out.println("WTF?!");
+        if (third.marry(second)) System.out.println(third.name + " & " + second.name + " Marryed!!!");
+            else System.out.println("У " + third.name + " и " + second.name + " Свадьбы не будет!!!");
+        if (third.marry(first)) System.out.println(third.name + " " + first.name + " Marryed!!!");
+            else{
+            if (first.divorce()) System.out.println(first.name + " divorced");
+            third.marry(first);
+            System.out.println(first.name + " & " + third.name + " Marryed!!!");
+
+        }
     }
 }
